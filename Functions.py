284e1a4c591638d7ee0eae26ef65a6eba759630b
TyGuy54 <81783445+TyGuy54@users.noreply.github.com>
2021-09-07 08:19:48 -0400

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,12 @@
+def fizz_buzz(numbers):
+
+    if (numbers % 3 == 0) and (numbers % 5 == 0):
+        return "FizzBuzz"
+    if numbers % 3 == 0:
+        return "Fizz"
+    if numbers % 5 == 0:
+        return "Buzz"
+    return numbers
+
+print(fizz_buzz(45))
+
